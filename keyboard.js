ca0f5bacb4fe0bb9f632cca69ff47ab32e694901
Marc <iq77@users.noreply.github.com>
2015-09-24 13:04:40 +0300

    Update keyboard.js

--- keyboard.js
+++ keyboard.js
@@ -1,26 +1,3 @@
-/*
- * synchronous use (poll for pressed keys in your code):
- *
- * var k = new Keyboard('standard');
- * [code...]
- * if (k.pressed.ESC)
- *    // then ESC is currently pressed
- *
- *
- * asynchronous use (call back whenever a key is being pressed):
- *
- * var k = new Keyboard('standard');
- * k.addHandler('ESC', 'esc-handler-1', function(){your code});
- * k.addHandler('ESC', 'esc-handler-1', function(){your code}); // overwrite previous...
- * k.removeHandler('ESC', 'esc-handler-1'); // remove previous
- *
- *
- *
- * check...
- * - PrntScrn -> keyup only in most browsers?
- *
- */
-
 
 function KeyboardHandler(name, callback, event){
     // event: "keydown" (default), "keyup" or "both"
